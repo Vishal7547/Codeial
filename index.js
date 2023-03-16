b9f0394b1408950a6b149b6d4f9018143fab2020
Vishal kumar <vishalk80269@gmail.com>
2023-03-16 09:37:33 +0530

    add some pages od ejs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 // use express router
 app.use('/' , require('./routes'));
+
+// setup the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
